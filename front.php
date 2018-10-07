51631d1794aa2c10edabb2a6a91b2d3a0f8f4494
Predyy-10 <sahrawat.anant@gmail.com>
2018-10-07 10:46:32 +0530

    added new files

---
+++ front.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+<title>We care about you!</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<div class="container-fluid text-center" id="container1">
+	<h1>Talk to Us. We care about You !!!!</h1>
+	<p>Feeling Alone? Call us, Chat with us, we are here to listen to you. 
+	 Tollfree No. +91-22-27546669 (24*7)
+	</p> 
+</div>
+<?php 
+include "navbar.php"; 
+?>
+</body>
+</html>
