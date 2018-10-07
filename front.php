848829c9bcc084a25765b0eb2879a79516c64f6a
Predyy-10 <sahrawat.anant@gmail.com>
2018-10-07 13:50:18 +0530

    new commit

--- front.php
+++ front.php
@@ -9,6 +9,7 @@
 
 <!-- jQuery library -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="script.js"></script>
 
 <!-- Latest compiled JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -25,6 +26,8 @@
 </div>
 <?php 
 include "navbar.php";
+include "chatbox.php";
 ?>
+
 </body>
 </html>
