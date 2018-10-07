69e153875c511580cb5427964849d913027e5f92
beerbottles <>
2018-10-07 10:40:15 +0530

    added navbar

---
+++ navbar.php
@@ -0,0 +1,20 @@
+
+<nav class="navbar navbar-fixed-top">
+<div class="container">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar" style="background-color: red; -webkit-box-shadow:0 5px 15px; box-shadow:0 5px 15px;">
+        <span style="color: white">Menu</span>                        
+</button>
+
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Posts</a></li>
+        <li><a href="#">Contact Us</a></li>
+		<li><a href="#">Gallery</a></li>
+	</ul>
+      
+    </div>
+</div>
+</div>
+</nav>
